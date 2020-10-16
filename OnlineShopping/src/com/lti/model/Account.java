7fe0246aabc2228f96669dac38e4320255048d28
Rahmans <Rahmans@RAHMAN>
2020-10-16 09:44:24 +0530

    account added in br1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,18 @@
+package com.lti.model;
+
+public class Account {
+	int accountId;
+	double accountBal;
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public double getAccountBal() {
+		return accountBal;
+	}
+	public void setAccountBal(double accountBal) {
+		this.accountBal = accountBal;
+	}
+}
