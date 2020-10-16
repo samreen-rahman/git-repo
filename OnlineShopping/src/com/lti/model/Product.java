9bdac83fa7e4b6cd2dc00f7848032512e8b3ba29
Rahmans <Rahmans@RAHMAN>
2020-10-16 09:28:04 +0530

    product class through br1

---
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -0,0 +1,27 @@
+package com.lti.model;
+
+public class Product {
+	int productId;
+	String productName;
+	String productDisciption;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDisciption() {
+		return productDisciption;
+	}
+	public void setProductDisciption(String productDisciption) {
+		this.productDisciption = productDisciption;
+	}
+	
+	
+}
